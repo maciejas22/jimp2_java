9896415097305b84e518649b0fa34eda6a6281ca
maciejas22 <87901496+maciejas22@users.noreply.github.com>
2022-05-26 21:23:19 +0200

    priority queue hotfix

--- app/src/algorithms/dijkstra.java
+++ app/src/algorithms/dijkstra.java
@@ -8,28 +8,24 @@ import java.util.PriorityQueue;
 
 public class dijkstra {
 
-    private double distance[];
-    private Vertex previous[];
+    private double[] distance;
+    private Vertex[] previous;
     private PriorityQueue<Edge> prioqueue;
-    private boolean is_visited[];
 
-    public Vertex[] findPath(Graph graf, Vertex src){
+    public double[] findPath(Graph graf, Vertex src){
         previous = new Vertex[graf.getColumns() * graf.getRows()];
         prioqueue = new PriorityQueue<Edge>(6, new Edge());
-        is_visited = new boolean[graf.getColumns() * graf.getRows()];
 
         distance = new double[graf.getColumns() * graf.getRows()];
         for(int i = 0; i < graf.getColumns() * graf.getRows(); i++){
             distance[i] = Double.MAX_VALUE;
             previous[i] = new Vertex();
-            for(int j = 0; j < graf.adjacency_list.get(i).getAdjacentEdges().size();j++)
-                prioqueue.add(graf.adjacency_list.get(i).getAdjacentEdges().get(j));
         }
         distance[src.getIndex()] = 0;
 
         Edge current_edge;
-        Vertex current_vertex = src;
         double tmp_distance = -1;
+        prioqueue.add(new Edge(src, 0));
 
         while(!prioqueue.isEmpty()){
             current_edge = prioqueue.remove();
@@ -38,9 +34,10 @@ public class dijkstra {
                 if(tmp_distance < distance[e.getDestination().getIndex()]){
                     distance[e.getDestination().getIndex()] = tmp_distance;
                     previous[e.getDestination().getIndex()] = current_edge.getDestination();
+                    prioqueue.add(e);
                 }
             }
         }
-        return previous;
+        return distance;
     }
 }
