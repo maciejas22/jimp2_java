c184517bbb69c9f91896f45c33d62be44f56353e
maciejas22 <87901496+maciejas22@users.noreply.github.com>
2022-06-01 20:26:37 +0200

    usuniecie niepotrzebnych importow

--- app/src/GUI/App.java
+++ app/src/GUI/App.java
@@ -3,14 +3,9 @@ package GUI;
 import java.io.IOException;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class App extends Application {
