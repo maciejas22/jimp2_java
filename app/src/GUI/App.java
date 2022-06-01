50bf338df4696c35e455992ac03091c1988cdd3c
maciejas22 <87901496+maciejas22@users.noreply.github.com>
2022-06-01 17:16:17 +0200

    fxml fix

--- app/src/GUI/App.java
+++ app/src/GUI/App.java
@@ -9,6 +9,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
@@ -16,9 +17,8 @@ public class App extends Application {
   @Override
   public void start(Stage primaryStage) {
 
-    Parent root;
     try {
-      root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
+      AnchorPane root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
       Scene scene = new Scene(root);
       primaryStage.setTitle("Graph Analyzer vlo.2");
       primaryStage.setScene(scene);
