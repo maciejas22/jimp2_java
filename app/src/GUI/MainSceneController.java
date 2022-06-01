022bc07641689f0d901fe53062a2a86f4ab91108
maciejas22 <87901496+maciejas22@users.noreply.github.com>
2022-06-01 20:27:02 +0200

    obsluga bledow, przypisanie guzikom ich funkcji

--- app/src/GUI/MainSceneController.java
+++ app/src/GUI/MainSceneController.java
@@ -1,12 +1,10 @@
 package GUI;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -15,14 +13,20 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
-import javafx.stage.Stage;
+
+import graphStructure.Graph;
+import graphStructure.Vertex;
+import algorithms.BFS;
+import algorithms.generator;
+import file_io.readFile;
+import file_io.writeFile;
 
 public class MainSceneController {
 
-    int rows,columns,DixtraStart,DixtraEnd;
-    double WeightMini,WeightMaxi;
-    String SavedFile, ReadFile;
-
+    private int rows,columns,DixtraStart,DixtraEnd;
+    private double WeightMini,WeightMaxi;
+    private String SavedFile, ReadFile;
+    private Graph graf = null;
 
     @FXML
     private TextField DoWDix;
@@ -59,12 +63,31 @@ public class MainSceneController {
 
     @FXML
     void GenerujClicked(ActionEvent event) throws IOException {
-        rows=Integer.parseInt(Wiersze.getText());
-        columns=Integer.parseInt(Kolumny.getText());
-        WeightMini=Double.parseDouble(WMini.getText());
-        WeightMaxi=Double.parseDouble(WMax.getText());
+        if(!Kolumny.getText().isEmpty() && !Wiersze.getText().isEmpty() && !WMax.getText().isEmpty() && !WMini.getText().isEmpty()){
+            try {
+            columns = Integer.parseInt(Kolumny.getText());
+            rows = Integer.parseInt(Wiersze.getText());
+            WeightMaxi = Double.parseDouble(WMax.getText());
+            WeightMini = Double.parseDouble(WMini.getText());
+            }
+            catch(NumberFormatException e){
+                Info.setText("Wprowadzono niepoprawne dane");
+                return;
+            }
+        }
+        else{
+            Info.setText("Wprowadz wszystkie dane.\n");
+            return;
+        }
+        
+        if(columns < 1 || rows < 1 || WeightMaxi < 0 || WeightMini < 0 || WeightMaxi < WeightMini){
+            Info.setText("Wprowadzone dane są niepoprawne.\n");
+            return;
+        }
+
         Info.appendText("Generujemy graf o wielkość "+rows+"x"+columns+"\n");
         Info.appendText("Minimalna waga wynasi: "+WeightMini+" zaś maxymalna: "+WeightMaxi+"\n");
+        graf = new generator().generateGraph(rows, columns, WeightMini, WeightMaxi);
 
         int diameterX, diameterY;
         int height = 590, width = 590;
@@ -90,30 +113,112 @@ public class MainSceneController {
         
         windowForGraph.getChildren().clear();
         windowForGraph.getChildren().addAll(edges, nodes);
+
     }
 
     @FXML
-    void ZapiszClicked(ActionEvent event) {
-        SavedFile=PlikZapisz.getText();
-        Info.appendText("Graf został zapisany do pliku o nazwie: "+SavedFile+".txt \n");
+    void ZapiszClicked(ActionEvent event) throws IOException {
+        if(!PlikZapisz.getText().isEmpty()){
+            SavedFile = PlikZapisz.getText();
+        }
+        else{
+            Info.appendText("Wprowadz nazwe pliku.\n");
+            return;
+        }
+
+        if(!SavedFile.contains(".")){
+            SavedFile += ".txt";
+        }
+
+        if(graf != null){
+           new writeFile().writeGraphToFile(graf, SavedFile);
+           Info.appendText("Graf został zapisany do pliku o nazwie: "+ SavedFile+ "\n");
+           new file_io.writeFile().writeGraphToFile(graf, SavedFile);
+        }
+        else {
+            Info.appendText("Najpierw wygeneruj graf! \n");
+        }
     }
 
     @FXML
-    void WczytajClicked(ActionEvent event) {
+    void WczytajClicked(ActionEvent event) throws IOException {
+        if(!PlikTxt.getText().isEmpty()){
+            ReadFile = PlikTxt.getText();
+            Info.appendText("Wczytuje graf z pliku o nazwie: "+ReadFile+"\n");
+            graf = new readFile().readGraphFromFile(ReadFile);
+        }
+        else{
+            Info.appendText("Wprowadz nazwe pliku.\n");
+            return;
+        }
         ReadFile=PlikTxt.getText();
-        Info.appendText("Graf został wczytany z pliku o nazwie: "+ReadFile+"\n");
     }
 
     @FXML
     void BFSClicked(ActionEvent event) {
-
+        if(graf != null){
+            Info.appendText("Wykonujemy algorytm BFS\n");
+            boolean isConnected = new BFS().isConnected(graf);
+            if(isConnected){
+                Info.appendText("Graf jest spójny\n");
+            }
+            else {
+                Info.appendText("Graf nie jest spójny\n");
+            }
+        }
+        else {
+            Info.appendText("Najpierw wygeneruj graf! \n");
+        }
     }
 
     @FXML
     void DijkstraClicked(ActionEvent event) {
-        DixtraStart=Integer.parseInt(ZWDix.getText());
-        DixtraEnd=Integer.parseInt(DoWDix.getText());
-        Info.appendText("Droga z wieszchołka nr "+DixtraStart+" do wieszchołka nr "+DixtraEnd+" ...\n");
+        if(ZWDix.getText().isEmpty() || DoWDix.getText().isEmpty()){
+            Info.appendText("Wprowadz wszystkie dane.\n");
+            return;
+        }
+        try {
+            DixtraStart = Integer.parseInt(ZWDix.getText());
+            DixtraEnd = Integer.parseInt(DoWDix.getText());
+        }
+        catch(NumberFormatException e){
+            Info.appendText("Wprowadzono niepoprawne dane.\n");
+            return;
+        }
+        
+        if(graf != null){
+            Info.appendText("Wykonujemy algorytm Dijkstry\n");
+        }
+        else {
+            Info.appendText("Najpierw wygeneruj graf! \n");
+            return;
+        }
+
+        if(!new BFS().isConnected(graf)){
+            Info.appendText("Graf nie jest spójny! \n");
+        }
+
+        if(DixtraStart < 0 || DixtraStart >= graf.adjacency_list.size() || DixtraEnd < 0 || DixtraEnd >= graf.adjacency_list.size()){
+            Info.appendText("Wprowadzono niepoprawne dane.\n");
+            return;
+        }
+
+        ArrayList<Integer> trasa = new ArrayList<Integer>();
+        Vertex[] path = new algorithms.dijkstra().findPath(graf, graf.adjacency_list.get(DixtraStart));
+        Vertex current = graf.adjacency_list.get(DixtraEnd);
+        while(current != graf.adjacency_list.get(DixtraStart)){
+            trasa.add(current.getIndex());
+            current = path[current.getIndex()];
+        }
+        trasa.add(DixtraStart);
+        Collections.reverse(trasa);
+
+        Info.appendText("Droga z wieszchołka nr "+DixtraStart+" do wieszchołka nr "+DixtraEnd+" \n");
+        for(int i = 0; i < trasa.size(); i++){
+            Info.appendText(trasa.get(i)+" ");
+        }
+        Info.appendText("\n");
+
     }
 
     @FXML
